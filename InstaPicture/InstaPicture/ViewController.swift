509034fafc4478c1df7dcaaf7eedc47a27d31a80
Ashish <happycoder29@gmail.com>
2023-04-08 14:20:57 -0700

    Checking in code

---
+++ InstaPicture/InstaPicture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaPicture
+//
+//  Created by Drillmaps on 08/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
